36e5b4be5a2ac93a99806b139a1adaae911b3e01
radhe721 <radhewebtech@gmail.com>
2024-08-29 00:41:57 +0530

    Algorithm Sum of Three Number Example Added

---
+++ Algorithm/Examplealgorithmsum.java
@@ -0,0 +1,20 @@
+// Print Average of 3 Given Numbers.
+// Let's Say You Want to write algorithm for it. So, the steps would be-
+// 1.Perform sum of 3 numbers.
+// 2.Store it in a variable sum.
+// 3.Divide the sum by 3.
+// 4.Store the value in variable avg.
+// 5.Print the value stored in avg.
+
+public class Examplealgorithmsum{
+    public static void main(String[] args){
+        int a,b,c;
+        a = 5;
+        b = 4;
+        c = 3;
+        int sum = a+b+c; // 5+4+3 = 12
+        int avg = sum/3; // 12/3 = 4
+        System.out.println(avg); //ans. 4
+
+    }
+}
